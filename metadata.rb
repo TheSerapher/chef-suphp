75ecb3927aceb2540fef5c89c9c46677845dbc66
Sebastian Grewe <sebastian@grewe.ca>
2013-10-09 11:49:48 +0200

    [VERSION] 0.1.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sebastian@grewe.ca'
 license          'Apache 2.0'
 description      'Installs/Configures suphp for Apache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
 %w{ php build-essential apache2 }.each do |cb|
   depends cb

eab9355e486c621a4886f3d8bae4e89dc8eabfa7
Sebastian Grewe <sebastian.grewe@xing.com>
2016-02-16 19:40:31 +0100

    [FIX] Another CentOS 7 thing

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,11 +3,16 @@ when 'rhel'
   default['suphp']['cookbooks'] = %w{ php apache2 build-essential }
   default['suphp']['packages'] = %w{ libtool php-devel httpd-devel }
   default['suphp']['install_method'] = 'source'
-  default['suphp']['apxs'] = '/usr/sbin/apxs'
+  if node['platform_version'].to_f >= 7
+    default['suphp']['apxs'] = '/usr/bin/apxs'
+  else
+    default['suphp']['apxs'] = '/usr/sbin/apxs'
+  end
 when 'debian'
   default['suphp']['cookbooks'] = %w{ php apache2 }
   default['suphp']['packages'] = %w{ libapache2-mod-suphp }
 end
+default['suphp']['configure_opts'] = "--prefix=/usr --sysconfdir=/etc --with-apr=/usr/bin/apr-1-config --with-apxs=#{node['suphp']['apxs']} --with-apache-user=#{node['apache']['user']} --with-setid-mode=paranoid --with-logfile=#{node['apache']['log_dir']}/suphp_log"
 default['suphp']['configure_opts'] = "--prefix=/usr --sysconfdir=/etc --with-apr=/usr/bin/apr-1-config --with-apxs=#{node['suphp']['apxs']} --with-apache-user=#{node['apache']['user']} --with-setid-mode=paranoid --with-logfile=#{node['apache']['log_dir']}/suphp_log"
 default['suphp']['url'] = 'http://suphp.org/download/'
 default['suphp']['version'] = '0.7.2'
